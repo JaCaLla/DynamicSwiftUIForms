c7f8e5cf2eeb1c5f76ee909f81ada78bb5939015
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2025-01-12 12:24:54 +0100

    iOS blank project created

---
+++ DynamicSwiftUIForms/DynamicSwiftUIFormsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicSwiftUIFormsApp.swift
+//  DynamicSwiftUIForms
+//
+//  Created by Javier Calatrava on 12/1/25.
+//
+
+import SwiftUI
+
+@main
+struct DynamicSwiftUIFormsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
